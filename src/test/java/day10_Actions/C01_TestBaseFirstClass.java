6fa45c5e53728798d63ed7ca85ad1cf9e61fa050
Oner Tekin <tekinoner37@gmail.com>
2022-05-19 21:08:51 +0300

    19.05.2022

---
+++ src/test/java/day10_Actions/C01_TestBaseFirstClass.java
@@ -0,0 +1,12 @@
+package day10_Actions;
+
+import org.junit.Test;
+import utilities.TestBase;
+
+public class C01_TestBaseFirstClass extends TestBase {
+    @Test
+    public void test01(){
+        driver.get("https://www.amazon.com");
+
+    }
+}
