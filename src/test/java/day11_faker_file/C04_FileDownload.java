aa4432fcf8e38da9d8be7e3dc598dbfe4e157245
Oner Tekin <tekinoner37@gmail.com>
2022-05-20 22:59:05 +0300

    20.05.2022

---
+++ src/test/java/day11_faker_file/C04_FileDownload.java
@@ -0,0 +1,34 @@
+package day11_faker_file;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import utilities.TestBase;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class C04_FileDownload extends TestBase {
+
+    @Test
+
+    public void test() throws InterruptedException {
+        //1. Tests packagenin altina bir class oluşturalim
+        //2. https://the-internet.herokuapp.com/download adresine gidelim.
+        //3. dummy.txt dosyasını indirelim
+        //4. dosyanın başarıyla indirilip indirilmediğini test edelim
+        driver.get("https://the-internet.herokuapp.com/download");
+        driver.findElement(By.xpath("//*[text()='dummy.txt']")).click();
+        Thread.sleep(5000);
+        //dosya downloads a indirilir
+        String farkliKisim=System.getProperty("user.home");
+        String ortakKisim="\\Downloads\\dummy.txt";
+        String dosyaYolu=farkliKisim+ortakKisim;
+
+        Assert.assertTrue(Files.exists(Paths.get(dosyaYolu)));
+
+
+
+
+    }
+}
