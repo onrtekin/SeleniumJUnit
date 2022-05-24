749f2bca44e17e4a91542d31eb25a29f964202d2
Oner Tekin <tekinoner37@gmail.com>
2022-05-24 23:18:23 +0300

    24.05.2022

--- src/test/java/day14_excel/C05_ReadExcel.java
+++ src/test/java/day14_excel/C05_ReadExcel.java
@@ -39,5 +39,7 @@ public class C05_ReadExcel {
             ulkelerMap.put(key,value);
         }
         System.out.println(ulkelerMap);
+        // Listede Ghana oldugunu test edelim
+        Assert.assertTrue(ulkelerMap.keySet().contains("Ghana"));
     }
 }
