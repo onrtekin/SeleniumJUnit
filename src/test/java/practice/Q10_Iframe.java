c4923d67a88bbff137d503952812b6874b8074a4
Oner Tekin <tekinoner37@gmail.com>
2022-05-24 19:56:30 +0300

    24.05.2022

--- src/test/java/practice/Q10_Iframe.java
+++ src/test/java/practice/Q10_Iframe.java
@@ -22,23 +22,23 @@ public class Q10_Iframe  extends TestBase {
     @Test
     public void test() throws InterruptedException {
         driver.get("https://www.jqueryscript.net/demo/Easy-iFrame-based-Twitter-Emoji-Picker-Plugin-jQuery-Emoojis/");
-        WebElement iFrame=driver.findElement(By.xpath("//iframe[@id='emoojis']"));
-        driver.switchTo().frame(iFrame);
-        driver.findElement(By.xpath("(//span[@class='mdl-tabs__ripple-container mdl-js-ripple-effect'])[2]")).click();
-
-       List<WebElement> emoojiler=driver.findElements(By.xpath("//div[@id='nature']"));
-       emoojiler.stream().forEach(t->t.click());
-       Thread.sleep(2000);
-
-       driver.switchTo().parentFrame();
-
-       List<WebElement> textList=driver.findElements(By.xpath("//div[@class='mdl-textfield mdl-js-textfield mdl-textfield--floating-label is-upgraded']"));
-       List<String> text=new ArrayList<>(Arrays.asList("1","2","3","4","5","6","7","8","9"));
-        for (int i = 0; i <textList.size() ; i++) {
+        WebElement iframe = driver.findElement(By.xpath("//iframe[@id ='emoojis']"));
+        driver.switchTo().frame(iframe);
+        WebElement secondEmoji = driver.findElement(By.xpath("(//span[@class='mdl-tabs__ripple-container mdl-js-ripple-effect'])[2]"));
+        secondEmoji.click();
+        List<WebElement> emojiOgeler = driver.findElements(By.xpath("//div[@id='nature']//div//img"));
+        // emojiOgeler.stream().forEach(x-> x.click());
+        for(WebElement each : emojiOgeler){
+            each.click();
+        }
+        Thread.sleep(3000);
+        driver.switchTo().parentFrame();
+        List<WebElement> textList = driver.findElements(By.xpath("//input[@class='mdl-textfield__input']"));
+        List<String> text = new ArrayList<>(Arrays.asList("Bir", "iframe" , "sorusu", "bu", "kadar", "eglenceli", "olabilir","sizce de ", "oyle degil mi ?", "",""));
+        for (int i=0; i< textList.size();i++){
             textList.get(i).sendKeys(text.get(i));
         }
+        Thread.sleep(3000);
         driver.findElement(By.xpath("//button[@id='send']")).click();
-
-
     }
 }
