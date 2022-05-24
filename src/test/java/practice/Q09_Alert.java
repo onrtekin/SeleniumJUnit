62dee98ae649891f7790e15e62fb96dcea4da6cd
Oner Tekin <tekinoner37@gmail.com>
2022-05-24 19:42:30 +0300

    24.05.2022

--- src/test/java/practice/Q09_Alert.java
+++ src/test/java/practice/Q09_Alert.java
@@ -1,6 +1,11 @@
 package practice;
 
-public class Q09_Alert {
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import utilities.TestBase;
+
+public class Q09_Alert extends TestBase {
 
     //    go to url :http://demo.automationtesting.in/Alerts.html
     //    click  "Alert with OK" and click 'click the button to display an alert box:'
@@ -11,5 +16,24 @@ public class Q09_Alert {
     //    and then sendKeys 'TechProEducation' (Please enter your name)
     //    finally print on console this message "Hello TechproEducation How are you today"
 
-
+    @Test
+    public void test() throws InterruptedException {
+        driver.get("http://demo.automationtesting.in/Alerts.html");
+        driver.findElement(By.xpath("(//a[@class='analystic'])[1]")).click();
+        driver.findElement(By.xpath("//button[@class='btn btn-danger']")).click();
+        System.out.println(driver.switchTo().alert().getText());
+        driver.switchTo().alert().accept();
+        driver.findElement(By.xpath("//*[text()='Alert with OK & Cancel ']")).click();
+        driver.findElement(By.xpath("//button[@class='btn btn-primary']")).click();
+        System.out.println(driver.switchTo().alert().getText());
+        driver.switchTo().alert().dismiss();
+        driver.findElement(By.xpath("(//a[@class='analystic'])[3]")).click();
+        driver.findElement(By.xpath("//button[@class='btn btn-info']")).click();
+        Thread.sleep(2000);
+        driver.switchTo().alert().sendKeys("TechProEducation");
+        driver.switchTo().alert().accept();
+        String expectedMsj = "Hello TechProEducation How are you today";
+        String actualMsj = driver.findElement(By.xpath("//p[@id='demo1']")).getText();
+        Assert.assertEquals(expectedMsj, actualMsj);
+    }
 }
--- src/test/java/practice/Q10_Iframe.java
+++ src/test/java/practice/Q10_Iframe.java
@@ -1,7 +1,14 @@
 package practice;
 
-public class Q10_Iframe  {
-      /*
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import utilities.TestBase;
+
+import java.util.List;
+
+public class Q10_Iframe  extends TestBase {
+
     // https://www.jqueryscript.net/demo/Easy-iFrame-based-Twitter-Emoji-Picker-Plugin-jQuery-Emoojis/ sitesine gidin
     // web sitesini maximize yapin
     // ikinci emojiye tıklayın
@@ -9,5 +16,20 @@ public class Q10_Iframe  {
     // parent iframe e geri donun
     // formu doldurun,(Formu istediğiniz metinlerle doldurun)
     //  apply button a basin
- */
+
+    @Test
+    public void test() throws InterruptedException {
+        driver.get("https://www.jqueryscript.net/demo/Easy-iFrame-based-Twitter-Emoji-Picker-Plugin-jQuery-Emoojis/");
+        WebElement iFrame=driver.findElement(By.xpath("//iframe[@id='emoojis']"));
+        driver.switchTo().frame(iFrame);
+        driver.findElement(By.xpath("(//span[@class='mdl-tabs__ripple-container mdl-js-ripple-effect'])[2]")).click();
+
+       List<WebElement> emoojiler=driver.findElements(By.xpath("//div[@id='nature']"));
+       emoojiler.stream().forEach(t->t.click());
+       Thread.sleep(2000);
+
+       driver.switchTo().parentFrame();
+
+
+    }
 }
